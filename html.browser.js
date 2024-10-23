337674e0b8a52eb5384137fcbe2a3695a9bbb6bd
Vinson Chuong <vinsonchuong@gmail.com>
2024-10-22 17:01:49 -0700

    feat(html): Also export makeHtml and makeCss for the browser

--- html.browser.js
+++ html.browser.js
@@ -1,4 +1,6 @@
 import {makeHtml, makeCss} from './lib/html/index.js'
 
+export {makeHtml, makeCss} from './lib/html/index.js'
+
 export const html = makeHtml(window)
 export const css = makeCss(window)
