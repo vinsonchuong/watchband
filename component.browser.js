fedc12c597306d6ad35d00d8827253738d2c3a79
Vinson Chuong <vinsonchuong@gmail.com>
2025-01-20 12:37:27 -0800

    feat(component): Add base component class
    - Add Component#ask, which emits a context-request event for dependency injection

--- component.browser.js
+++ component.browser.js
@@ -1,6 +1,6 @@
 import {registerComponent as baseRegisterComponent} from './lib/web-component/index.js'
 
-export {render} from './lib/web-component/index.js'
+export {render, Component} from './lib/web-component/index.js'
 export * from './lib/web-component/templates.js'
 export * from './lib/signal/index.js'
 export * from './lib/observable/index.js'
