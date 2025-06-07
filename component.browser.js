77b6a4b5805acee085b736d8b77c0985afdab6fe
Vinson Chuong <vinsonchuong@gmail.com>
2025-06-07 13:05:49 -0700

    fix(ssr): Fix various bugs in server-side rendering
    - Promote `HtmlTemplate` and `CssTemplate` to `lib/html`
    - Provide a generic way to render templates
    - Provide the render function and arguments in the render context
    - Refactor downstream code to use new template rendering

--- component.browser.js
+++ component.browser.js
@@ -1,7 +1,7 @@
 import {registerComponent as baseRegisterComponent} from './lib/web-component/index.js'
 
 export {render, Component} from './lib/web-component/index.js'
-export * from './lib/web-component/templates.js'
+export * from './lib/html/template/index.js'
 export * from './lib/signal/index.js'
 export * from './lib/observable/index.js'
 
